761b0054337ccd2dad7ac1f5af773f9533df3161
Sourav Snigdha Mansingh <souravsnigdh@gmail.com>
2025-12-27 22:32:53 +0530

    Implemented a class

---
+++ LLD - Golang/OOP/class/car.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// Car struct (similar to class)
+type Car struct {
+	brand string // unexpected (private to package)
+	model string
+	speed int
+}
+
+// Constructor-like function
+func NewCar(brand, model string) *Car {
+	return &Car{
+		brand: brand,
+		model: model,
+		speed: 0,
+	}
+}
+
+// Method to accelerate
+func (c *Car) Accelerate(increment int) {
+	c.speed += increment
+}
+
+// Method to display status
+func (c *Car) DisplayStatus() {
+	fmt.Printf("%s is running at %d km/h.\n", c.brand, c.speed)
+}
+
+func main() {
+	car := NewCar("BMW", "X5")
+	car.Accelerate(50)
+	car.DisplayStatus()
+}
