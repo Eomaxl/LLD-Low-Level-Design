16dfe5ef6e3834b57b447271aece4c6be3755946
Sourav Snigdha Mansingh <souravsnigdh@gmail.com>
2026-01-09 22:25:24 +0530

    ANother version of parking lot

---
+++ LLD - Golang/Easy/parking-lot-v2/internal/parkinglot/enums.go
@@ -0,0 +1,17 @@
+package parkinglot
+
+type SpotType int
+
+const (
+	SpotMotorcycle = iota
+	SpotCar
+	SpotLarge
+)
+
+type VehicleType int
+
+const (
+	VehicleMotorcycle = iota
+	VehicleCar
+	VehicleLarge
+)
